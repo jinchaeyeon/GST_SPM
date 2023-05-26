d109bad9d98401619ea55124f80138057117c571
YUJIN-G <yjgo235@gmail.com>
2023-05-26 09:50:32 +0900

    회의록 열람 - 상세 정보 추가

--- src/routes/MeetingView.tsx
+++ src/routes/MeetingView.tsx
@@ -4,7 +4,6 @@ import {
   DatePicker,
   DatePickerChangeEvent,
 } from "@progress/kendo-react-dateinputs";
-import { Editor, EditorUtils } from "@progress/kendo-react-editor";
 import {
   getSelectedState,
   Grid,
@@ -15,18 +14,14 @@ import {
 } from "@progress/kendo-react-grid";
 import { Input, InputChangeEvent } from "@progress/kendo-react-inputs";
 import { bytesToBase64 } from "byte-base64";
-import React, {
-  useState,
-  CSSProperties,
-  useRef,
-  useEffect,
-  useCallback,
-} from "react";
+import React, { useState, CSSProperties, useRef, useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import {
   ButtonContainer,
   FilterBox,
   FilterBoxWrap,
+  FormBox,
+  FormBoxWrap,
   GridContainer,
   GridContainerWrap,
   GridTitle,
@@ -42,12 +37,15 @@ import {
   UseGetValueFromSessionItem,
   UseParaPc,
 } from "../components/CommonFunction";
-import { GAP, SELECTED_FIELD } from "../components/CommonString";
+import { GAP, PAGE_SIZE, SELECTED_FIELD } from "../components/CommonString";
 import RichEditor from "../components/RichEditor";
 import { useApi } from "../hooks/api";
 import { isLoading } from "../store/atoms";
 import { TEditorHandle } from "../store/types";
-const PAGE_SIZE = 100;
+import { IAttachmentData } from "../hooks/interfaces";
+import AttachmentsWindow from "../components/Windows/CommonWindows/AttachmentsWindow";
+import CenterCell from "../components/Cells/CenterCell";
+
 const styles: { [key: string]: CSSProperties } = {
   table: {
     borderCollapse: "collapse",
@@ -83,6 +81,9 @@ const App = () => {
   const [meetingnum, setMeetingnum] = useState("");
   UseParaPc(setPc);
   const userId = UseGetValueFromSessionItem("user_id");
+
+  const [attachmentsWindowVisible, setAttachmentsWindowVisible] =
+    useState<boolean>(false);
 
   //조회조건 Input Change 함수 => 사용자가 Input에 입력한 값을 조회 파라미터로 세팅
   const filterInputChange = (e: DatePickerChangeEvent | InputChangeEvent) => {
@@ -130,7 +131,7 @@ const App = () => {
       const link = document.createElement("a");
       link.href = URL.createObjectURL(file);
       link.download =
-        detailDataResult[0].title + (isRef ? "_참고자료" : "") + ".doc";
+        detailDataLines[0].title + (isRef ? "_참고자료" : "") + ".doc";
       link.click();
     }
   };
@@ -150,7 +151,12 @@ const App = () => {
   const [mainDataResult, setMainDataResult] = useState<DataResult>(
     process([], mainDataState)
   );
-  const [detailDataResult, setDetailDataResult] = useState<any[]>([]);
+  const [detailDataLines, setDetailDataLines] = useState<any[]>([]);
+  const [detailData, setDetailData] = useState({
+    meetingnum: "",
+    attdatnum: "",
+    files: "",
+  });
 
   const [selectedState, setSelectedState] = useState<{
     [id: string]: boolean | number[];
@@ -172,6 +178,11 @@ const App = () => {
     const selectedIdx = event.startRowIndex;
     const selectedRowData = event.dataItems[selectedIdx];
 
+    setDetailData({
+      meetingnum: selectedRowData[DATA_ITEM_KEY],
+      attdatnum: selectedRowData.attdatnum,
+      files: selectedRowData.files,
+    });
     setMeetingnum(selectedRowData[DATA_ITEM_KEY]);
   };
 
@@ -281,7 +292,7 @@ const App = () => {
       if (editorRef.current) {
         editorRef.current.setHtml(reference);
       }
-      setDetailDataResult(rows);
+      setDetailDataLines(rows);
     } else {
       console.log("[에러발생]");
       console.log(data);
@@ -289,7 +300,7 @@ const App = () => {
       if (editorRef.current) {
         editorRef.current.setHtml("");
       }
-      setDetailDataResult([]);
+      setDetailDataLines([]);
     }
     setLoading(false);
   };
@@ -368,6 +379,16 @@ const App = () => {
     }
   }, [meetingnum]);
 
+  const getAttachmentsData = (data: IAttachmentData) => {
+    setDetailData((prev) => ({
+      ...prev,
+      attdatnum: data.attdatnum,
+      files:
+        data.original_name +
+        (data.rowCount > 1 ? " 등 " + String(data.rowCount) + "건" : ""),
+    }));
+  };
+
   return (
     <>
       <TitleContainer>
@@ -430,7 +451,7 @@ const App = () => {
             <GridTitle>회의록 리스트</GridTitle>
           </GridTitleContainer>
           <Grid
-            style={{ height: `100%` }}
+            style={{ height: `calc(100% - 35px)` }}
             data={process(
               mainDataResult.data.map((row) => ({
                 ...row,
@@ -461,15 +482,47 @@ const App = () => {
             //컬럼너비조정
             resizable={true}
           >
-            <GridColumn field="custnm" title="업체" />
-            <GridColumn field="recdt" title="회의일" />
-            <GridColumn field="title" title="제목" />
+            <GridColumn
+              field="recdt"
+              title="회의일"
+              width={100}
+              cell={CenterCell}
+            />
+            <GridColumn field="custnm" title="업체" width={120} />
+            <GridColumn field="title" title="제목" width={300} />
           </Grid>
         </GridContainer>
         <GridContainer width={`calc(50% - ${GAP}px)`}>
           <GridTitleContainer>
             <GridTitle>회의록</GridTitle>
           </GridTitleContainer>
+          <FormBoxWrap border>
+            <FormBox>
+              <tbody>
+                <tr>
+                  <th>회의록 번호</th>
+                  <td>
+                    <Input
+                      name="meetingnum"
+                      value={detailData.meetingnum}
+                      className="readonly"
+                    />
+                  </td>
+                  <th style={{ width: 0 }}>첨부파일</th>
+                  <td style={{ width: "auto" }}>
+                    <div className="filter-item-wrap">
+                      <Input name="attachment_q" value={detailData.files} />
+                      <Button
+                        icon="more-horizontal"
+                        fillMode={"flat"}
+                        onClick={() => setAttachmentsWindowVisible(true)}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </FormBox>
+          </FormBoxWrap>
           <div
             id="htmlContent"
             style={{
@@ -479,7 +532,7 @@ const App = () => {
               height: "100%",
             }}
           >
-            {detailDataResult.length > 0 && (
+            {detailDataLines.length > 0 && (
               <>
                 <h1
                   style={{
@@ -517,11 +570,11 @@ const App = () => {
                   <tr>
                     <th style={styles.th}>회의 일자</th>
                     <td style={styles.td} width="30%">
-                      {detailDataResult[0].date}
+                      {detailDataLines[0].date}
                     </td>
                     <th style={styles.th}>작성자</th>
                     <td style={styles.td} width="30%">
-                      {detailDataResult[0].user_name}
+                      {detailDataLines[0].user_name}
                     </td>
                   </tr>
                   <tr>
@@ -534,7 +587,7 @@ const App = () => {
                       }}
                       colSpan={3}
                     >
-                      {detailDataResult[0].place}
+                      {detailDataLines[0].place}
                     </td>
                   </tr>
                   <tr>
@@ -547,7 +600,7 @@ const App = () => {
                       }}
                       colSpan={3}
                     >
-                      {detailDataResult[0].title}
+                      {detailDataLines[0].title}
                     </td>
                   </tr>
                   <tr>
@@ -567,7 +620,7 @@ const App = () => {
                       }}
                       colSpan={4}
                     >
-                      {detailDataResult.map((row) => (
+                      {detailDataLines.map((row) => (
                         <p key={row.meetingseq}>{row.contents}</p>
                       ))}
                     </td>
@@ -580,19 +633,20 @@ const App = () => {
         <GridContainer width={`calc(40% - ${GAP}px)`}>
           <GridTitleContainer>
             <GridTitle>참고자료</GridTitle>
-            {/* <Button
-              icon={"file-word"}
-              name="reference"
-              onClick={downloadWordDoc}
-              themeColor={"primary"}
-              fillMode={"outline"}
-            >
-              다운로드
-            </Button> */}
           </GridTitleContainer>
           <RichEditor id="editor" ref={editorRef} readonly />
         </GridContainer>
       </GridContainerWrap>
+
+      {attachmentsWindowVisible && (
+        <AttachmentsWindow
+          type="meeting"
+          setVisible={setAttachmentsWindowVisible}
+          setData={getAttachmentsData}
+          para={detailData.attdatnum}
+          permission={{ upload: false, download: true, delete: false }}
+        />
+      )}
     </>
   );
 };
